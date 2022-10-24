cce10e17b9dbf58fdea0ad109c9a0f233ecfe521
Yulia Saidak <yluya.guo@gmail.com>
2022-10-24 20:18:47 +0300

    adding new dependencies

--- src/main/java/com/company/hotel_booking/ContextConfiguration.java
+++ src/main/java/com/company/hotel_booking/ContextConfiguration.java
@@ -1,5 +1,7 @@
 package com.company.hotel_booking;
 
+import com.company.hotel_booking.utils.managers.MessageManager;
+import com.company.hotel_booking.web.interceptor.GeneralInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +11,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.Locale;
 
 /**
  * Class Spring configuration
@@ -22,7 +31,7 @@ import javax.persistence.Persistence;
 @EnableTransactionManagement
 @EnableJpaRepositories
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-public class ContextConfiguration {
+public class ContextConfiguration extends WebMvcConfigurationSupport {
 
     @Bean
     public EntityManagerFactory entityManagerFactory() {
@@ -33,4 +42,42 @@ public class ContextConfiguration {
     public TransactionManager transactionManager() {
         return new JpaTransactionManager(entityManagerFactory());
     }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("css/**", "images/**", "js/**")
+                .addResourceLocations("classpath:/static/css/", "classpath:/static/images/", "classpath:/static/js/");
+    }
+
+    @Bean
+    public MessageManager messageManager() {
+        return new MessageManager(Locale.UK);
+    }
+
+    @Bean
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Bean
+    public GeneralInterceptor generalInterceptor() {
+        return new GeneralInterceptor();
+    }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(generalInterceptor())
+                .addPathPatterns("/**");
+    }
+
+
 }
