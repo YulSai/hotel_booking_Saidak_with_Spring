cac27b54737713c3081a5e4a06ea3d78e83beb1d
Yulia Saidak <yluya.guo@gmail.com>
2022-10-25 02:14:03 +0300

    format filters and add entry-point

--- src/main/java/com/company/hotel_booking/web/filters/AuthorizationFilter.java
+++ src/main/java/com/company/hotel_booking/web/filters/AuthorizationFilter.java
@@ -2,10 +2,10 @@ package com.company.hotel_booking.web.filters;
 
 import com.company.hotel_booking.utils.aspects.logging.annotations.LogInvocation;
 import com.company.hotel_booking.utils.managers.MessageManager;
+import lombok.RequiredArgsConstructor;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,18 +15,16 @@ import java.io.IOException;
 /**
  * Class with filter for authorization
  */
-@WebFilter(urlPatterns = {"/users/all", "/users/{id}", "/users/update", "/users/delete", "/users/change_password",
-        "/rooms/all", "/rooms/{id}", "/rooms/update", "/rooms/create", "/reservations/all", "/reservations/{id}",
-        "/reservations/update", "/reservations/create", "/reservations/cancel_reservation",
-        "/reservations/user_reservations"})
+@RequiredArgsConstructor
 public class AuthorizationFilter extends HttpFilter {
+    private final MessageManager messageManager;
     @Override
     @LogInvocation
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
             throws IOException, ServletException {
         HttpSession session = req.getSession(false);
         if (session == null || session.getAttribute("user") == null) {
-            req.setAttribute("message", MessageManager.getMessage("msg.login"));
+            req.setAttribute("message", messageManager.getMessage("msg.login"));
             res.sendRedirect("/login");
             return;
         }
