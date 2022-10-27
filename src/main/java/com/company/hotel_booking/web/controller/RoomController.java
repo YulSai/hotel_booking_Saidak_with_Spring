d62d2cdcc443811f02a10b800b55315e54600954
Yulia Saidak <yluya.guo@gmail.com>
2022-10-28 02:14:39 +0300

    adding validation to controllers

--- src/main/java/com/company/hotel_booking/web/controller/RoomController.java
+++ src/main/java/com/company/hotel_booking/web/controller/RoomController.java
@@ -5,14 +5,15 @@ import com.company.hotel_booking.service.api.RoomService;
 import com.company.hotel_booking.service.dto.RoomDto;
 import com.company.hotel_booking.utils.aspects.logging.annotations.LogInvocation;
 import com.company.hotel_booking.utils.aspects.logging.annotations.NotFoundEx;
-import com.company.hotel_booking.utils.managers.MessageManager;
 import com.company.hotel_booking.utils.managers.PagesManager;
-import com.company.hotel_booking.web.controller.util.PagingUtil;
+import com.company.hotel_booking.web.controller.utils.PagingUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,8 +23,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/rooms")
@@ -31,7 +34,7 @@ import java.util.List;
 public class RoomController {
     private final RoomService roomService;
     private final PagingUtil pagingUtil;
-    private final MessageManager messageManager;
+    private final MessageSource messageManager;
 
     @LogInvocation
     @NotFoundEx
@@ -55,6 +58,11 @@ public class RoomController {
         return PagesManager.PAGE_ROOM;
     }
 
+    @ModelAttribute
+    public RoomDto roomDto(){
+        return new RoomDto();
+    }
+
     @LogInvocation
     @GetMapping("/create")
     public String createRoomForm() {
@@ -63,9 +71,12 @@ public class RoomController {
 
     @LogInvocation
     @PostMapping("/create")
-    public String createRoom(@ModelAttribute RoomDto room) {
-        RoomDto created = roomService.create(room);
-        // model.addAttribute("message", MessageManager.getMessage("msg.room.created"));
+    public String createRoom(@ModelAttribute @Valid RoomDto roomDto, Errors errors, HttpSession session, Locale locale) {
+        if (errors.hasErrors()){
+            return PagesManager.PAGE_CREATE_ROOM;
+        }
+        RoomDto created = roomService.create(roomDto);
+        session.setAttribute("message", messageManager.getMessage("msg.room.created", null, locale));
         return "redirect:/rooms/" + created.getId();
     }
 
@@ -79,9 +90,13 @@ public class RoomController {
 
     @LogInvocation
     @PostMapping("/update/{id}")
-    public String updateRoom(@ModelAttribute RoomDto room, Model model) {
-        RoomDto updated = roomService.update(room);
-        model.addAttribute("message", messageManager.getMessage("msg.room.updated"));
+    public String updateRoom(@ModelAttribute @Valid RoomDto roomDto, Errors errors, HttpSession session,
+                             Locale locale) {
+        if (errors.hasErrors()){
+            return PagesManager.PAGE_UPDATE_ROOM;
+        }
+        RoomDto updated = roomService.update(roomDto);
+        session.setAttribute("message", messageManager.getMessage("msg.room.updated", null, locale));
         return "redirect:/rooms/" + updated.getId();
     }
 
@@ -95,18 +110,20 @@ public class RoomController {
     @PostMapping("/search_available_rooms")
     public String searchAvailableRoom(HttpSession session, @RequestParam String check_in,
                                       @RequestParam String check_out, @RequestParam String type,
-                                      @RequestParam String capacity, Model model) {
+                                      @RequestParam String capacity, Model model, Locale locale) {
         LocalDate checkIn = LocalDate.parse(check_in);
         LocalDate checkOut = LocalDate.parse(check_out);
         if (checkOut.equals(checkIn) | checkOut.isBefore(checkIn)) {
-            model.addAttribute("message", messageManager.getMessage("msg.incorrect.date"));
+            model.addAttribute("message", messageManager
+                    .getMessage("msg.incorrect.date", null, locale));
             return PagesManager.PAGE_SEARCH_AVAILABLE_ROOMS;
         } else {
             Long typeId = Room.RoomType.valueOf(type.toUpperCase()).getId();
             Long capacityId = Room.Capacity.valueOf(capacity.toUpperCase()).getId();
             List<RoomDto> roomsAvailable = roomService.findAvailableRooms(typeId, capacityId, checkIn, checkOut);
             if (roomsAvailable.isEmpty()) {
-                model.addAttribute("message", messageManager.getMessage("msg.search.no.available.rooms"));
+                model.addAttribute("message", messageManager
+                        .getMessage("msg.search.no.available.rooms", null, locale));
             }
             session.setAttribute("rooms_available", roomsAvailable);
             session.setAttribute("check_in", checkIn);
@@ -118,12 +135,6 @@ public class RoomController {
     @LogInvocation
     @PostMapping("/rooms_available")
     public String getAvailableRoom() {
-        return PagesManager.PAGE_ROOMS_AVAILABLE;
-    }
-
-    @LogInvocation
-    @GetMapping("/rooms_available")
-    public String getAvailableRoomR() {
         return PagesManager.PAGE_ROOMS_AVAILABLE;
     }
 }
